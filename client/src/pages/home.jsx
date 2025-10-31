1ea8d31d8951dd49138f5bdfaaac1f43bfb46142
Anurag Basuri <anuragbasuri2502@gmail.com>
2025-10-31 09:09:58 +0530

    refactor(Home): remove ThemeToggle and Background3D components for cleaner layout

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -5,19 +5,11 @@ import EventsPreview from '../components/EventsPreview.jsx';
 import TeamPreview from '../components/TeamPreview.jsx';
 import Footer from '../components/Footer.jsx';
 import AboutSyntax from '../components/AboutSyntax.jsx';
-import ThemeToggle from '../components/ThemeToggle.jsx';
-import Background3D from '../components/Background3D.jsx';
 
 const Home = () => {
 	return (
 		<div className="relative min-h-screen bg-transparent overflow-x-hidden text-primary">
-			{/* Themed, Next-like 3D background */}
-			<Background3D />
-
-			{/* Theme toggle */}
-			<ThemeToggle />
-
-			{/* Content remains transparent */}
+			{/* Content only; background is global */}
 			<div className="relative z-10">
 				<Hero />
 				<AboutSyntax />
